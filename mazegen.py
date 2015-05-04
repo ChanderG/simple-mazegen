bc70e04b233991b1d6e13046f5bae33423695ad3
ChanderG <chandergovind@gmail.com>
2015-05-04 16:12:11 +0530

    complete python mazegen

--- mazegen.py
+++ mazegen.py
@@ -1,11 +1,32 @@
 import sys
+from random import shuffle, randrange
 
 def maze_gen(w = 10, h = 5):
-  visited = [[0] * w + [1]] * h + [[1] * (w+1)]
-  hor = [['+--'] * w + ['+']] * (h + 1)
-  ver = [['|  '] * (w+1)] * h + [[]]
+  # extreme syntax - * is used to duplicate
+  # rewrite with dictionary as 2d matrix
+  visited = [[0] * (w) + [1] for _ in range(h)]  + [[1] * (w+1)]
+  hor = [['+--'] * (w) + ['+'] for _ in range(h + 1)] 
+  ver = [['|  '] * (w+1) for _ in range(h)] + [[]]
 
-  print visited    
+  def visit_room(x, y):
+    visited[y][x] = 1
+
+    neighbours = [(x, y-1), (x, y+1), (x+1, y), (x-1, y)]
+    shuffle(neighbours)
+
+    for (xx, yy) in neighbours:
+      if visited[yy][xx] == 1:
+	#print "Visited ", xx, yy
+	continue
+      if xx == x: hor[max(yy, y)][x] = '+  '
+      if yy == y: ver[y][max(xx,x)] = '   '
+      visit_room(xx, yy)	
+    
+    return
+
+  visit_room(randrange(w),randrange(h))
+  #visit_room(0,0)
+
   for (a,b) in zip(hor, ver):
     for i in a:
       sys.stdout.write(i)
@@ -14,4 +35,4 @@ def maze_gen(w = 10, h = 5):
       sys.stdout.write(j)
     print 
 
-maze_gen()
+maze_gen(16, 8)

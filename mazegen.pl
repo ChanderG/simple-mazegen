9b51429e1dd5abd7459a8ec2504c35b3fcdb39ea
ChanderG <chandergovind@gmail.com>
2015-05-07 17:54:21 +0530

    add code for printing maze

--- mazegen.pl
+++ mazegen.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 
 # helper subs
+# simple 1d array print sub
 sub printArray {
   my @array = @_;   # as an array is passed in
   foreach (@array) {
@@ -10,6 +11,7 @@ sub printArray {
   print "\n";
 }
 
+# simple 2d array print sub
 sub print2dArray {
   my @array = @_;
   foreach (@array) {
@@ -32,6 +34,21 @@ my @visited = map [@row], 1 .. $h;
 push @visited, [map '1', 0 .. $w];
 
 print2dArray @visited;
+
+my @hor = map [map '+--', 1 .. $w], 0 .. $h;
+my @ver = map [map '|  ', 1 .. $w], 1 .. $h;
+
+print2dArray @hor;
+print2dArray @ver;
+
+for my $i (0 .. $h-1) {
+  print "@{$hor[$i]}",'+';
+  print "\n";
+  print "@{$ver[$i]}",'|';
+  print "\n";
+}
+print "@{$hor[$h]}",'+';
+print "\n";
 
 sub visit_room {
   my ($x, $y) = $_;
